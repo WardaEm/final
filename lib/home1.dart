d820ec52e24372c4374376ec0b3e3864ca6d14d4
Warda Emad <wardaemad32@std.mans.edu.eg>
2023-01-26 02:00:59 +0200

    first

--- lib/home1.dart
+++ lib/home1.dart
@@ -10,12 +10,14 @@ class Home1 extends StatefulWidget {
 
 class _Home1State extends State<Home1> {
   TextEditingController?controller;
+
 @override
   void initState() {
   controller=TextEditingController();
   // TODO: implement initState
     super.initState();
   }
+  // var FormKey=GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,8 +29,9 @@ class _Home1State extends State<Home1> {
                crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-              child:  Image.asset('assets/v.png',fit: BoxFit.cover,width: 428,height: 380,),
-                margin:EdgeInsets.fromLTRB(0, 0, 0,0) ,
+              child:  Image.asset('assets/v.png',fit: BoxFit.cover,width: 428,
+                height: 360,),
+                // margin:EdgeInsets.fromLTRB(0, 0, 0,0) ,
                 // DropdownButtonHideUnderline(
                 //   child: DropdownButton <String>(value: value,
                 //     items: items.map(buildMenuItem).toList(),
@@ -38,7 +41,7 @@ class _Home1State extends State<Home1> {
                 // )
 
                 ),
-                // SizedBox(height: 8,),
+
                 Padding(
                   padding: const EdgeInsets.all(15.0),
                   child: Row(
@@ -47,17 +50,82 @@ class _Home1State extends State<Home1> {
                     children: [
                       Image.asset('assets/vo.png',width: 18,height:9 ,),
                       SizedBox(width: 5,),
-                      Text("cameras",style: TextStyle(fontSize:24,fontWeight: FontWeight.w400 ),),
+                      Text("cameras",style: TextStyle(
+                          fontSize:24,fontWeight: FontWeight.w400 ),),
                     ],
                   ),
                 ),
+
+                  Container(
+                    padding: EdgeInsets.only(left: 15,top: 10,right: 100,),
+
+                    child: TextFormField(textAlign: TextAlign.center,
+                        controller:controller ,
+
+                        validator:(value)=>value!.isEmpty?"This field is reuired":null
+                      ,decoration: InputDecoration(hintText: "Enter Place",hintStyle:
+                        TextStyle(
+                          fontSize: 21,color: Color(0xff00101D),
+                        ),fillColor: Colors.white,focusColor: Colors.white54
+                            ,hoverColor: Colors.white,focusedBorder: OutlineInputBorder(),
+                            enabled: true,
+                            disabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)),
+                            enabledBorder: OutlineInputBorder(
+                            borderSide:BorderSide(color: Color(0xff00101D)),
+                            borderRadius:BorderRadius.circular(12)))),
+
+                  ),
+                SizedBox(height: 20,),
                 Padding(
-                  padding: const EdgeInsets.only(left: 15,top:10,right: 100),
-                  child: TextFormField(textAlign: TextAlign.center,controller:controller ,validator:(value)=>value!.isEmpty?"This field is reuired":null
-                    ,decoration: InputDecoration(hintText: "Enter Place",hintStyle: TextStyle(
-                        fontSize: 24,color: Color(0xff00101D)
-                      ),label:Text("ewr"),enabledBorder: OutlineInputBorder(borderSide:BorderSide(color: Color(0xff00101D)),borderRadius:BorderRadius.circular(12)))),
-                ),
+                  padding: const EdgeInsets.only(left: 15,right:55 ),
+                  child: Container(
+                    width: 366,
+height: 62,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid
+                      )
+                    ),
+                    child: Row(
+
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+TextButton(autofocus:false,style: ButtonStyle(
+
+),onPressed: (){
+  print('dfghjik');
+}, child: Text('Tap here to add your camera',
+  style: TextStyle(
+fontSize: 20,fontWeight: FontWeight.w500,fontFamily: "Poppins",
+                          color: Color(0xff00101D)
+                        ),)),
+//                         Text("Tap here to add your camera",style: TextStyle(
+// fontSize: 20.1,fontWeight: FontWeight.w500,fontFamily: "Poppins",
+//                           color: Color(0xff00101D)
+//                         ),
+//                         ),
+
+
+                        FloatingActionButton(hoverColor: Color(0xffff8fcfd),splashColor:  Color(0xffff8fcfd),
+                          highlightElevation: 0,focusElevation: 0,hoverElevation: 0,
+                          disabledElevation: 0,
+                          focusColor: Color(0xffff8fcfd),elevation: 0,
+                          backgroundColor: Color(0xfff8fcfd),onPressed: (){
+                          setState(() {
+print('jm bv x');
+                          });
+                        },child: Image.asset("assets/plus.png",width: 18,
+                          height: 18,),mini: true,),
+
+                      ],
+                    ),
+                  ),
+                )
+                // ),
 
 
               ]
